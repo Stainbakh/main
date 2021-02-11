7c40c36a851e982f980d2389743f77ee15e365e1
Ivan <stainbakh@mail.ru>
2021-02-11 23:15:54 +0300

    расширение

---
+++ src/main/java/ru/stainbakh/main/lab1/Palindrome.java
@@ -0,0 +1,9 @@
+package ru.stainbakh.main.lab1;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String sь ь ь ь ь ь ь = args[i];
+        }
+    }
+}
